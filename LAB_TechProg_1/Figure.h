959922e3149a51c5f3332db55713f1057deef598
Andrey13-cyber <pescherov13@gmail.com>
2021-10-14 15:04:29 +0300

    Добавьте файлы проекта.

---
+++ LAB_TechProg_1/Figure.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<string>
+#include<iostream>
+using namespace std;
+
+class Figure
+{
+public:
+    string type;
+    double size;
+    double square;
+    /*double perimeter;*/
+
+};
+
---
+++ LAB_TechProg_1/Figure3D.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "Figure.h"
+class Figure3D :
+    public Figure
+{
+};
+
